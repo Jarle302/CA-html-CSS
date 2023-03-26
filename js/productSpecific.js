df1ed1fad3eb9f47602349173e51bb96789085ee
Jarle302 <107812980+Jarle302@users.noreply.github.com>
2023-03-26 19:36:50 +0200

    update

--- js/productSpecific.js
+++ js/productSpecific.js
@@ -41,9 +41,7 @@ function renderSpecific(data, index) {
                     }$</p>
                   </div>
                   <div class="product-specific__container--buttons">
-                  <a class="btn btn--second" href="your-order.html?index=${jacketIndex}"
-                    >Your order</a
-                  >
+                 
                   <button id="addToCartButton" class="btn product-specific__button--cta" href="your-order.html?index=${jacketIndex}"
                     >Add to Cart </button>
                     
